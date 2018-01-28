3c1163835f026aa1d98b122afbad536b85cbefdf
Thomas Pircher <thp-web@p5r.uk>
2018-01-28 18:41:28 +0000

    Add support for various WiFi models (merge from master)
    TRENDnet TEW-809UB
    ASUS USB-AC68
    Netgear A7000
    TRENDnet TEW-809UB
    Edimax AC1750

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -207,11 +207,16 @@ static struct usb_device_id rtw_usb_id_tbl[] ={
 	{USB_DEVICE(USB_VENDER_ID_REALTEK, 0x8813),.driver_info = RTL8814A},
 	{USB_DEVICE(0x2001, 0x331a), .driver_info = RTL8814A}, /* D-Link - D-Link */
 	{USB_DEVICE(0x0b05, 0x1817), .driver_info = RTL8814A}, /* ASUS - ASUSTeK */
+	{USB_DEVICE(0x0b05, 0x1852), .driver_info = RTL8814A}, /* ASUS - ASUSTeK */
 	{USB_DEVICE(0x056E, 0x400B), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
 	{USB_DEVICE(0x056E, 0x400D), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
+	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - Edimax */
 	{USB_DEVICE(0x7392, 0xA834), .driver_info = RTL8814A}, /* Edimax - Edimax */
 	{USB_DEVICE(0x20f4, 0x809a), .driver_info = RTL8814A}, /* TRENDnet - TRENDnet */
-	{USB_DEVICE(0x2357, 0x0106), .driver_info = RTL8814A}, /* TP-LINK - TP-LINK */
+	{USB_DEVICE(0x20f4, 0x809b), .driver_info = RTL8814A}, /* TRENDnet TEW-809UB */
+	{USB_DEVICE(0x2357, 0x0106), .driver_info = RTL8814A}, /* TP-LINK Archer T9UH */
+	{USB_DEVICE(0x0846, 0x9054), .driver_info = RTL8814A}, /* Netgear A7000 */
+	{USB_DEVICE(0x20f4, 0x809b), .driver_info = RTL8814A}, /* TRENDnet TEW-809UB */
 #endif /* CONFIG_RTL8814A */
 
 #ifdef CONFIG_RTL8188F
