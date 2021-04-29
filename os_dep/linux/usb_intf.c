faca9b2b0f57b77550c603b447f63474d7fbc8fc
Diederik de Haas <github@cknow.org>
2021-04-29 16:11:20 +0200

    Sort and add some device IDs.
    Signed-off-by: Diederik de Haas <github@cknow.org>

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -215,12 +215,18 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 #ifdef CONFIG_RTL8814A
 
 	{USB_DEVICE(USB_VENDER_ID_REALTEK, 0x8813), .driver_info = RTL8814A},
-	{USB_DEVICE(0x2001, 0x331a), .driver_info = RTL8814A}, /* D-Link - D-Link */
-	{USB_DEVICE(0x0b05, 0x1817), .driver_info = RTL8814A}, /* ASUS - ASUSTeK */
 	{USB_DEVICE(0x056E, 0x400B), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
 	{USB_DEVICE(0x056E, 0x400D), .driver_info = RTL8814A}, /* ELECOM - ELECOM */
+	{USB_DEVICE(0x0846, 0x9054), .driver_info = RTL8814A}, /* Netgear A7000 */
+	{USB_DEVICE(0x0b05, 0x1817), .driver_info = RTL8814A}, /* ASUS - ASUSTeK */
+	{USB_DEVICE(0x0b05, 0x1852), .driver_info = RTL8814A}, /* ASUS - AC68 (new product ID) */
+	{USB_DEVICE(0x0b05, 0x1853), .driver_info = RTL8814A}, /* ASUS - AC68 (another product ID) */
+	{USB_DEVICE(0x2001, 0x331a), .driver_info = RTL8814A}, /* D-Link - D-Link */
+	{USB_DEVICE(0x20f4, 0x809a), .driver_info = RTL8814A}, /* TRENDnet - TRENDnet */
+	{USB_DEVICE(0x20f4, 0x809b), .driver_info = RTL8814A}, /* TRENDnet - TEW-809UB */
+	{USB_DEVICE(0x2357, 0x0106), .driver_info = RTL8814A}, /* TP-Link Archer T9UH */
+	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - AC1750 */
 	{USB_DEVICE(0x7392, 0xA834), .driver_info = RTL8814A}, /* Edimax - Edimax */
-	{USB_DEVICE(0x7392, 0xA833), .driver_info = RTL8814A}, /* Edimax - Edimax */
 #endif /* CONFIG_RTL8814A */
 
 #ifdef CONFIG_RTL8188F
